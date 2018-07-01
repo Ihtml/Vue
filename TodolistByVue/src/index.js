cc4b44299e38547db26dcba8a1cb44f2bd3355b0
Jiang XiaoJun <2483690020@qq.com>
2018-07-01 21:23:13 +0800

    modify and add comment

--- TodolistByVue/src/index.js
+++ TodolistByVue/src/index.js
@@ -1,10 +1,11 @@
 import Vue from 'Vue'
-import app from './app.vue'
+import App from './app.vue'
 
+//创造一个节点
 const Root = document.createElement('div')
 document.body.appendChildTo(Root)
 
 let vm = new Vue({
 	//h是creatElement的别名
-	render: (h)=>h(app)
-	}).$monted(Root)
+	render: (h) = >h(App)
+	}).$mount(Root)//手动挂载
