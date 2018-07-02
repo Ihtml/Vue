ada1a1b6c1cf36ef3391da398c1afcd974f5e825
IFE2 <2483690020@qq.com>
2018-07-02 14:11:24 +0800

    add app.vue

---
+++ TodolistByVue/src/app.vue
@@ -0,0 +1,23 @@
+<template>
+	<div id="app">
+		<div id="cover"></div>
+		<Header></Header>
+		<Todo></Todo>
+		<Footer></Footer>
+	</div>
+</template>
+
+<script>
+	// 这个.vue组件不能直接在浏览器中运行
+	import Header from './todo/header.vue'
+	import Todo from './todo/todo.vue'
+	import Footer from './todo/footer.jsx'
+
+	export default{
+		components:{
+			Header,
+			Todo,
+			Footer,
+		}
+	}
+</script>
