3e5908a4a4587ff6d465386fd9d6184ca7c01e7a
Jiang XiaoJun <2483690020@qq.com>
2018-07-22 12:56:45 +0800

    modified

--- TodolistByVue/client/app.vue
+++ TodolistByVue/client/app.vue
@@ -1,58 +1,59 @@
 <template>
-	<div id="app">
-		<div id="cover"></div>
-		<Header></Header>
-		<!-- router-link相当于a标签 -->
+    <div id="app">
+        <div id="cover"></div>
+        <Header></Header>
+        <!-- router-link相当于a标签 -->
     <!-- 使用：to使Vue去解析它而不是当成字符串来处理 -->
-		<router-link :to="{name: 'app'}">app</router-link>
-		<router-link to='/login/user101'>login</router-link>
-		<!-- <Todo></Todo> -->
-		<!-- 加上过渡动画 -->
-		<transition name="fade">
+        <router-link :to="{name: 'app'}">app</router-link>
+        <router-link to='/login/user101'>login</router-link>
+        <!-- <Todo></Todo> -->
+        <!-- 加上过渡动画 -->
+        <transition name="fade">
     <!-- router-view是占位符,内容为路由component的内容 -->
-		<router-view/>
-		</transition>
-		<Footer></Footer>
-		<!-- 多个路由 -->
-		<router-view name="a"/>
-	</div>
+        <router-view/>
+        </transition>
+        <Footer></Footer>
+        <!-- 多个路由 -->
+        <router-view name="a"/>
+    </div>
 </template>
 
 <script>
-	// 这个.vue组件不能直接挂载到HTML上在浏览器中运行
-	import Header from './layout/header.vue'
-	import Todo from './views/todo/todo.vue'
-	import Footer from './layout/footer.jsx'
+// 这个.vue组件不能直接挂载到HTML上在浏览器中运行
+import Header from './layout/header.vue';
+import Todo from './views/todo/todo.vue';
+import Footer from './layout/footer.jsx';
 
-	export default{
-		components:{
-			Header,
-			Todo,
-			Footer,
-		},
-		mounted () {
-			console.log(this.$route)
-		}
-	}
+export default {
+  components: {
+    Header,
+    Todo,
+    Footer
+  },
+  mounted() {
+    console.log(this.$route);
+  }
+};
 </script>
 
 <!-- 加载组件时才会加载样式,节省流量 -->
 <style lang="stylus" scoped>
-    #app{
-        position absolute
-        left 0
-        right 0
-        top 0
-        bottom 0
-    }
-    #cover{
-        position absolute
-        left 0
-        top 0
-        right 0
-        bottom 0
-        background-color: #999
-        opacity .1
-        z-index -1
-    }
+#app {
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+}
+
+#cover {
+    position: absolute;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #999;
+    opacity: 0.1;
+    z-index: -1;
+}
 </style>
