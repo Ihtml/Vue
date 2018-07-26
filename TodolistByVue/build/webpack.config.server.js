a028cc3f8a379785053a5bbfc6d07f31a590c699
IFE2 <2483690020@qq.com>
2018-07-26 10:58:21 +0800

    服务端渲染配置

---
+++ TodolistByVue/build/webpack.config.server.js
@@ -0,0 +1,52 @@
+const Path = require('path')
+const Webpack = require('webpack')
+//这个包可以合并不同的webpack配置
+const Merge = require('webpack-merge')
+//把非js的的代码打包成一个静态资源文件
+const ExtractPlugin = require('extract-text-webpack-plugin')
+//引入webpack基本配置
+const BaseConfig = require('./webpack.config.base')
+const VueServerPlugin = require('vue-server-renderer/server-plugin')
+
+
+let config
+config = Merge(BaseConfig, {
+	// 打包出来的程序是在node端运行的
+	target: 'node',
+	entry: Path.join(__dirname, '../client/server-entry.js'),
+	devtool: 'source-map',
+	output: {
+		libraryTarget: 'commonjs2', // module.exports = 
+		filename: 'server-entry.js',
+		path: Path.join(__dirname, '../server-build')
+	},
+	externals: Object.keys(require('../package.json').dependencies),
+	module: {
+		rules: [{
+			test: /\.styl/,
+			use: ExtractPlugin.extract({
+				fallback: 'vue-style-loader',
+				use: [
+					'css-loader',
+					{
+						loader: 'postcss-loader',
+						options: {
+							sourceMap: true,
+						}
+					},
+					'stylus-loader'
+				]
+			})
+		}]
+	},
+	plugins: [
+		new ExtractPlugin('styles.[contentHash:8].css'),
+		new Webpack.DefinePlugin({
+			'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+			'process.env.VUE_ENV': '"server"',
+		}),
+		new VueServerPlugin()
+	]
+})
+
+module.exports = config
