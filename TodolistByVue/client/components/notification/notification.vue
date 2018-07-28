49b3ef7f1dfa3f0d5e2630b298c54524f866f888
Jiang XiaoJun <2483690020@qq.com>
2018-07-28 19:39:21 +0800

    新增组件

---
+++ TodolistByVue/client/components/notification/notification.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <span class="content">{{content}}</span>
+    <a class="btn">{{btn || '关闭'}}</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Notification',
+  props: {
+    content: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
