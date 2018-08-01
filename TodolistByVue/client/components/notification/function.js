5f195c123b19daf2cf8b6c03702f00be861d957c
IFE2 <2483690020@qq.com>
2018-08-01 10:07:22 +0800

    性能优化

--- TodolistByVue/client/components/notification/function.js
+++ TodolistByVue/client/components/notification/function.js
@@ -6,41 +6,60 @@ const NotificationConstructor = Vue.extend(Component)
 let seed = 1
 const instances = []
 
+const removeInstance = (instance) => {
+  if (!instance) return
+  const len = instances.length
+  const index = instances.findIndex(inst => instance.id === inst.id)
+
+  instances.splice(index, 1)
+  if (len <= 1) return
+  const removeHeight = instance.vm.height
+  for (let i = index; i < len - 1; i++) {
+    instances[i].verticalOffset =
+      parseInt(instances[i].verticalOffset) - removeHeight - 16
+  }
+}
+
 const notify = (options) => {
-	if (Vue.prototype.$isServer) return
+  if (Vue.prototype.$isServer) return
 
-	const {
-		autoClose,
-		...rest
-	} = options
-	const instance = new NotificationConstructor({
-		propsData: {
-			...rest
-		},
-		data: {
-			autoClose: autoClose === undefined ? 3000 : autoClose
-		}
-	})
+  const {
+    autoClose,
+    ...rest
+  } = options
+  const instance = new NotificationConstructor({
+    propsData: {
+      ...rest
+    },
+    data: {
+      autoClose: autoClose === undefined ? 3000 : autoClose
+    }
+  })
 
-	const id = `notification_${seed++}`
-	instance.id = id
-	// 只是生成了一个$el的对象，还没真正插入到DOM里面去
-	instance.vm = instance.$mount()
-	document.body.appendChild(instance.vm.$el)
-	instance.vm.visible = true
+  const id = `notification_${seed++}`
+  instance.id = id
+  // 只是生成了一个对象，还没真正插入到DOM里面去
+  instance.vm = instance.$mount()
+  document.body.appendChild(instance.vm.$el)
+  instance.vm.visible = true
 
-	let verticalOffset = 0
-	instances.forEach(item => {
-		verticalOffset += item.$el.offsetHeight + 16
-	})
-	verticalOffset += 16
-	instance.verticalOffset = verticalOffset
-	instances.push(instance)
+  let verticalOffset = 0
+  instances.forEach(item => {
+    verticalOffset += item.$el.offsetHeight + 16
+  })
+  verticalOffset += 16
+  instance.verticalOffset = verticalOffset
+  instances.push(instance)
 
-	instance.vm.$on('close', () => {
-		instance.vm.visible = false
-	})
-	return instance.vm
+  instance.vm.$on('closed', () => {
+    removeInstance(instance)
+    document.body.removeChild(instance.vm.$el)
+    instance.vm.$destory()
+  })
+  instance.vm.$on('close', () => {
+    instance.vm.visible = false
+  })
+  return instance.vm
 }
 
 export default notify
