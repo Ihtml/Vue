ec34fb7fa059bd59575260c355163fd045b1d885
IFE2 <2483690020@qq.com>
2018-08-21 15:34:41 +0800

    projefct init

--- Travel/src/main.js
+++ Travel/src/main.js
@@ -3,8 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 解决移动端click事件300ms延迟的问题
+import fastClick from 'fastclick'
+import './assets/styles/reset.css'
+// 解决高清屏1像素边框问题
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
