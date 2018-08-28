7a501638ef508dc141d5caf53b110a1d91bee795
Jiang XiaoJun <2483690020@qq.com>
2018-08-28 23:23:43 +0800

    add search-logic

--- Travel/src/pages/city/components/List.vue
+++ Travel/src/pages/city/components/List.vue
@@ -18,7 +18,7 @@
 
         </div>
       </div>
-      <div class="area" v-for="(item, key) of cities" :key="key">
+      <div class="area" v-for="(item, key) of cities" :key="key" :ref="key">
         <div class="title border-topbottom">{{key}}</div>
         <div class="item-list">
           <div class="item border-bottom" v-for="innerItem of item" :key="innerItem.id">{{innerItem.name}}</div>
@@ -35,10 +35,20 @@ export default {
   name: 'CityList',
   props: {
     cities: Object,
-    hot: Array
+    hot: Array,
+    letter: String
   },
-  mounted() {
+  mounted () {
     this.scroll = new Bscroll(this.$refs.wrapper)
+  },
+  watch: {
+    letter () {
+      if (this.letter) {
+        const element = this.$refs[this.letter][0]
+        // better-scroll提供的方法，滚动到对应DOM元素的位置
+        this.scroll.scrollToElement(element)
+      }
+    }
   }
 }
 </script>
