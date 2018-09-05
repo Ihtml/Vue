ed1dd825de674fea06faafbac61d5b9922bdf132
IFE2 <2483690020@qq.com>
2018-09-05 16:26:35 +0800

    add home-hot-list

--- Travel/src/pages/home/components/HotList.vue
+++ Travel/src/pages/home/components/HotList.vue
@@ -9,7 +9,67 @@
       </a>
     </div>
     <ul class="list">
-
+      <li class="item">
+        <a class="item-link" href="http://touch.piao.qunar.com/touch/detail.htm?id=6330&from=as_recommend_sight">
+          <div class="item-tag">
+            <img class="tag-img" src="http://img1.qunarzz.com/piao/fusion/1710/ab/159673b63e6ca702.png">
+          </div>
+          <div class="item-img">
+            <img src="http://img1.qunarzz.com/sight/p0/1508/f7/f7c3be996152011c.img.jpg_150x150_eab8097d.jpg">
+          </div>
+          <div class="item-sight">南京总统府</div>
+          <div class="item-price">
+            <span class="price-tag">￥</span>
+            <span class="price-num">35</span>
+            <span class="price-txt">起</span>
+          </div>
+        </a>
+      </li>
+      <li class="item">
+        <a class="item-link" href="http://touch.piao.qunar.com/touch/detail.htm?id=6330&from=as_recommend_sight">
+          <div class="item-tag">
+            <img class="tag-img" src="http://img1.qunarzz.com/piao/fusion/1710/2d/36d0c4adaebbbc02.png">
+          </div>
+          <div class="item-img">
+            <img src="http://img1.qunarzz.com/sight/p0/1508/f7/f7c3be996152011c.img.jpg_150x150_eab8097d.jpg">
+          </div>
+          <div class="item-sight">南京总统府</div>
+          <div class="item-price">
+            <span class="price-tag">￥</span>
+            <span class="price-num">35</span>
+            <span class="price-txt">起</span>
+          </div>
+        </a>
+      </li>
+      <li class="item">
+        <a class="item-link" href="http://touch.piao.qunar.com/touch/detail.htm?id=6330&from=as_recommend_sight">
+          <div class="item-tag">
+            <img src="http://img1.qunarzz.com/piao/fusion/1710/67/edc47ffef9e96b02.png" class="tag-img">
+          </div>
+          <div class="item-img">
+            <img src="http://img1.qunarzz.com/sight/p0/1508/f7/f7c3be996152011c.img.jpg_150x150_eab8097d.jpg">
+          </div>
+          <div class="item-sight">南京总统府</div>
+          <div class="item-price">
+            <span class="price-tag">￥</span>
+            <span class="price-num">35</span>
+            <span class="price-txt">起</span>
+          </div>
+        </a>
+      </li>
+      <li class="item">
+        <a class="item-link" href="http://touch.piao.qunar.com/touch/detail.htm?id=6330&from=as_recommend_sight">
+          <div class="item-img">
+            <img src="http://img1.qunarzz.com/sight/p0/1508/f7/f7c3be996152011c.img.jpg_150x150_eab8097d.jpg">
+          </div>
+          <div class="item-sight">南京总统府</div>
+          <div class="item-price">
+            <span class="price-tag">￥</span>
+            <span class="price-num">35</span>
+            <span class="price-txt">起</span>
+          </div>
+        </a>
+      </li>
     </ul>
   </div>
 </template>
@@ -21,6 +81,7 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+@import '~styles/mixins.styl'
 .hot-list
   margin-top 0.2rem
   background #fff
@@ -50,4 +111,60 @@ export default {
       line-height 0.28rem
       .title-more-icon
         margin-left 0.04rem
+  .list
+    padding 0 0.24rem
+    overflow-x scroll
+    white-space nowrap
+    font-size 0
+    .item
+      position relative
+      display inline-block
+      width 2rem
+      padding 0.06rem 0 0.2rem
+      &:not(:first-child)
+        margin-left 0.12rem
+      .item-link
+        display block
+        width 100%
+        height 100%
+        .tag-img
+          position absolute
+          top 0
+          left 0
+          z-index 1
+          overflow hidden
+          width 0.84rem
+          height 0.4rem
+        .item-img
+          overflow hidden
+          width 2rem
+          height 0
+          padding-bottom 100%
+          background #f0f5f8
+          img
+            width 100%
+        .item-sight
+          margin-top 0.12rem
+          color #212121
+          font-size 0.24rem
+          line-height 0.32rem
+          text-align center
+          ellipsis()
+        .item-price
+          color #616161
+          line-height 0.36rem
+          text-align center
+          font-size 0.24rem
+          display flex
+          justify-content center
+          align-items baseline
+          .price-tag
+            color #ff8300
+            font-size 0.24rem
+            margin-right -0.05rem
+          .price-num
+            color #ff8300
+            font-size 0.28rem
+          .price-txt
+            font-size 0.24rem
 </style>
